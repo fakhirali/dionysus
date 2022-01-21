c236b6693ef3e8ce4e36a5825e3339cefbab5616
fakhirali <ali.fakhir33@gmail.com>
2022-01-21 16:22:12 +0500

    changed PairwiseDistances for Cosine similarity Idk if it works

--- build/bindings/python/CMakeFiles/dionysus.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dionysus"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dionysus.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
